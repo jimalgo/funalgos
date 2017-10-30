21c01eb0bcd6cf7275af989338b84edcf56252a4
jimalgo <33237394+jimalgo@users.noreply.github.com>
2017-10-30 23:32:13 +0100

    Create index.js

---
+++ BoyerMooreMajority/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {array} list
+ * @returns {object} with key "hasMajority" and "majorityChat" (optional)
+ */
+exports.default = function(list) {
+  return {
+    hasMajority: false,
+  };
+}
