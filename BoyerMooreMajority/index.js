1f7b3a53285880a2ad638ea5b0a216008482d268
Filip Danić <filip@spicefactory.co>
2017-10-30 23:43:25 +0100

    implement Boyer Moore Majority algorithm

--- BoyerMooreMajority/index.js
+++ BoyerMooreMajority/index.js
@@ -1,9 +1,23 @@
 /**
  * @param {array} list
- * @returns {object} with key "hasMajority" and "majorityChat" (optional)
+ * @returns {object} with key "hasMajority" and "majorityChar" (optional)
  */
 exports.default = function(list) {
+  var char = null;
+  var counter = 0;
+  list.forEach((el) => {
+    if (counter === 0) {
+      counter = 1;
+      char = el;
+    } else if (char === el) {
+      counter += 1;
+    } else {
+      counter -= 1;
+    }
+  });
+
   return {
-    hasMajority: false,
+    hasMajority: counter > 0,
+    majorityChar: counter > 0 ? char : undefined,
   };
 }
